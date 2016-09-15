fb45513157b49eb5a53d1322c21c27fa6d73198d
Boris Burkov <b.burkov@gmail.com>
2016-09-15 10:28:59 +0000

    [libify] first whack at fs lib

--- lib/fs.cpp
+++ lib/fs.cpp
@@ -1,4 +1,5 @@
-#include <dirent.h>
+#include "fs.h"
+
 #include <syslog.h>
 #include <sys/stat.h>
 
@@ -8,9 +9,51 @@
 namespace lib {
 namespace fs {
 
-Directory::Directory(const std::string& path) : path(path) {}
+Directory::Directory(const std::string path) : path(path) {};
 
-File::File(const std::string& path) : path(path) {}
+void Directory::open() {
+  DIR* d;
+  if ((d = opendir(path.c_str())) == NULL) {
+    syslog(LOG_ERR, "Error opening directory %s: %m", path.c_str());
+    return;
+  }
+  dir_.reset(d);
+}
+
+bool Directory::readNext() {
+  if (!dir_) {
+    return false;
+  }
+  struct dirent* d = readdir(dir_.get());
+  if (!d) {
+    return false;
+  }
+  std::string p(d->d_name);
+  if (p == "." || p == "..") {
+    return true;;
+  }
+  children.push_back(p);
+  return true;
+}
+
+File::File(const std::string path) : path(path) {};
+
+void File::open() {
+  fin_ = std::make_unique<std::ifstream>(path, std::ifstream::in);
+}
+
+bool File::readNext() {
+  std::string line;
+  if (!fin_) {
+    return false;
+  }
+  auto b = getline(*fin_, line);
+  if (!b) {
+    return false;
+  }
+  lines.push_back(line);
+  return true;
+}
 
 } // namespace fs
 } // namespace lib
--- lib/fs.h
+++ lib/fs.h
@@ -1,17 +1,45 @@
+#include <dirent.h>
+
+#include <string>
+#include <vector>
+
 namespace lib {
 namespace fs {
 
-class Directory {
+template <class T>
+class File {
  public:
-  Directory(const std::string& path);
-  std::string& path;
+  void read() {
+    f().open();
+    while (f().readNext()) {
+      continue;
+    }
+  }
+ private:
+  T& f() { return *static_cast<T*>(this); }
 }
+
+class Directory : File<Directory>{
+ public:
+  Directory(const std::string path);
+  std::string& path;
+  void open();
+  bool readNext();
+  std::vector<std::string> children;
+ private:
+  std::unique_ptr<DIR> dir_;
+};
 
 class File {
  public:
-  File(const std::string& path);
+  File(const std::string path);
   std::string& path;
-}
+  void open();
+  bool readNext();
+  std::vector<std::string> lines;
+ private:
+  std::unique_ptr<std::ifstream> fin_;
+};
 
 } // namespace fs
 } // namespace lib
