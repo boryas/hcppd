6053429f0c2ed3f2078769eababca76747df6214
Boris Burkov <b.burkov@gmail.com>
2020-05-12 05:31:04 +0000

    v0 of binding a socket

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
+#include <error.h>
+#include <errno.h>
 #include <getopt.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 static const char * const ssfs_srv_usage[] = {
 	"usage: ssfs-srv [options]\n Runs the ssfs server",
@@ -16,13 +22,69 @@ void usage() {
   }
 }
 
+int getaddrinfo_for_bind(char *port, sa_family_t family,
+                         struct sockaddr_storage *saddr) {
+  int status;
+  struct addrinfo hints;
+  struct addrinfo *addrinfos;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+  hints.ai_family = family;
+  status = getaddrinfo(NULL, port, &hints, &addrinfos);
+  if (status != 0) {
+    error(0, 0, "Failed to create socket address: %s", gai_strerror(status));
+    goto out;
+  }
+  memmove(saddr, (struct sockaddr_storage *)addrinfos->ai_addr, sizeof(*saddr));
+out:
+  freeaddrinfo(addrinfos);
+  return status;
+}
+
+int serve(char *port, sa_family_t family) {
+  int sockfd; // socket we bind
+  int remotefd; // socket returned by accept
+  struct sockaddr_storage local;
+  struct sockaddr_storage remote;
+  socklen_t addrsize = sizeof(remote);
+  int status;
+
+  sockfd = socket(family, SOCK_STREAM, 0);
+  if (sockfd == -1) {
+    error(0, errno, "failed to create socket");
+    return errno;
+  }
+  status = getaddrinfo_for_bind(port, family, &local);
+  if (status) {
+    return status;
+  }
+
+  if (bind(sockfd, (struct sockaddr *)&local, sizeof(local)) == -1) {
+    error(0, errno, "failed to bind socket");
+    return errno;
+  }
+
+  if (listen(sockfd, 10) == -1) {
+    error(0, errno, "failed to listen on socket");
+    return errno;
+  }
+
+  remotefd = accept(sockfd, (struct sockaddr*)&remote, &addrsize);
+  if (remotefd == -1) {
+    error(0, errno, "failed to accept a connection");
+    return errno;
+  }
+  printf("Successfully accepted a connection!\n");
+  return 0;
+}
+
 int main(int argc, char **argv) {
   int c;
   optind = 0;
   
   int verbosity = 0;
-  int port = 80;
-  char *p_end;
+  char port[8] = "80";
 
   while (1) {
     static const struct option long_opts[] = {
@@ -43,11 +105,13 @@ int main(int argc, char **argv) {
       verbosity++;
       break;
     case 'p':
-      port = strtol(optarg, &p_end, 10);
+      memset(port, 0, sizeof(port));
+      strcpy(port, optarg);
       break;
     }
   }
 
-  printf("Starting ssfs-srv listening on port %d\n", port);
+  printf("Starting ssfs-srv listening on port %s\n", port);
+  serve(port, AF_INET6);
   return 0;
 };
